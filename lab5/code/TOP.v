4088df4d162d78888a082c4df54ca8ccd2904aac
lzh <lz1907041@gmail.com>
2024-05-03 09:28:44 +0800

    delete module 'ALL' and rewrite module 'TOP'

--- lab5/code/TOP.v
+++ lab5/code/TOP.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 2024/05/02 22:00:25
+// Create Date: 2024/05/03 09:01:00
 // Design Name: 
 // Module Name: TOP
 // Project Name: 
@@ -19,15 +19,32 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
-module TOP(
-    input clk_dm,
-    input Mem_Write,
-    input [7:2]DM_Addr,
-    input [31:0]M_W_Data,
-    output [31:0]M_R_Data
+module TOP (
+    input clk,//芯片时钟
+    input clk_dm,//存储器时钟
+    input Mem_Write,//读写信号
+    input [7:2]DM_Addr,//访问的地址
+    input [1:0]MW_Data_s,//开关，用于选择写入的数据
+    output [2:0]which,//位选
+    output [7:0]seg//段选
 );
 
-RAM_B MEM(clk_dm,Mem_Write,DM_Addr,M_W_Data,M_R_Data);
+wire [31:0]M_R_Data;//读数据
+reg [31:0]M_W_Data;//写数据
+
+always@(*)begin
+    if(Mem_Write)begin
+        case(MW_Data_s)
+            2'b00:M_W_Data=32'h12345678;
+            2'b01:M_W_Data=32'h87654321;
+            2'b10:M_W_Data=32'h00001111;
+            2'b11:M_W_Data=32'hFFFF1111;
+        endcase
+    end
+end//组合逻辑电路：当信号Mem_Write=1时，根据MW_Data_s选择M_W_Data
+
+RAM_B u1(clk_dm,Mem_Write,DM_Addr,M_W_Data,M_R_Data);
+
+DISPLAY u2(clk,M_R_Data,1,which,seg);
 
 endmodule
